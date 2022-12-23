10f0af8aede01a89285a74c9fa3a71835d9571ea
김희철 <kim11403@naver.com>
2022-12-23 23:11:47 +0900

    숨어있는 숫자의 덧셈(1)

---
+++ Lv_0/숨어있는숫자의덧셈_1.java
@@ -0,0 +1,11 @@
+class Solution {
+	public int solution(String my_string) {
+		int answer = 0;
+		for (int i = 0; i < my_string.length(); i++) {
+			if (Character.isDigit(my_string.charAt(i))) {
+				answer += (int) my_string.charAt(i) - 48;
+			}
+		}
+		return answer;
+	}
+}
